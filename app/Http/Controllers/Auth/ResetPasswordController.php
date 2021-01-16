e1ec15362471b7e462b01a33d64233d9c0bb4116
mazhar-rony <mazhar.rony@gmail.com>
2021-01-17 05:40:56 +0600

    multi auth logic with middleware

--- app/Http/Controllers/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Foundation\Auth\ResetsPasswords;
 
 class ResetPasswordController extends Controller
@@ -25,7 +26,8 @@ class ResetPasswordController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    //protected $redirectTo = '/home';
+    protected $redirectTo;
 
     /**
      * Create a new controller instance.
@@ -34,6 +36,15 @@ class ResetPasswordController extends Controller
      */
     public function __construct()
     {
+        if(Auth::check() && Auth::user()->role->id == 1)
+        {
+            $this->redirectTo = route('admin.dashboard');
+        }
+        else
+        {
+            $this->redirectTo = route('author.dashboard');
+        }
+
         $this->middleware('guest');
     }
 }
