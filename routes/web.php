83303cfab2dbd6e0cea74fdf3a6f4744bfddd66a
mazhar-rony <mazhar.rony@gmail.com>
2021-01-18 09:43:12 +0600

    layout design complete

--- routes/web.php
+++ routes/web.php
@@ -4,11 +4,11 @@
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('home');
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+// Route::get('/home', 'HomeController@index')->name('home');
 
 Route::group(['as' => 'admin.', 'prefix' => 'admin', 'namespace' => 'Admin', 'middleware' => ['auth', 'admin']], function () {
     Route::get('dashboard', 'DashboardController@index')->name('dashboard');
