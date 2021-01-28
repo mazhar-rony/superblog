59ad53e59da4ddf1cfe14cc319b1f1529f032ff1
mazhar-rony <mazhar.rony@gmail.com>
2021-01-28 07:16:36 +0600

    Favorite Post Adding

--- app/User.php
+++ app/User.php
@@ -46,4 +46,9 @@ class User extends Authenticatable
     {
         return $this->hasMany('App\Post');
     }
+
+    public function favorite_posts()
+    {
+        return $this->belongsToMany('App\Post')->withTimestamps();
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,10 @@ Auth::routes();
 
 // Route::get('/home', 'HomeController@index')->name('home');
 
+Route::group(['middleware' => ['auth']], function() {
+    Route::post('favorite/{post}/add', 'FavoriteController@add')->name('post.favorite');
+});
+
 Route::group(['as' => 'admin.', 'prefix' => 'admin', 'namespace' => 'Admin', 'middleware' => ['auth', 'admin']], function () {
     Route::get('dashboard', 'DashboardController@index')->name('dashboard');
 
@@ -38,6 +42,6 @@ Route::group(['as' => 'author.', 'prefix' => 'author', 'namespace' => 'Author', 
     Route::get('settings', 'SettingsController@index')->name('settings');
     Route::put('profile-update', 'SettingsController@updateProfile')->name('profile.update');
     Route::put('password-update', 'SettingsController@updatePassword')->name('password.update');
-    
+
     Route::resource('post', 'PostController');
 });
