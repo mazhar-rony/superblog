05727c59d0b0a5790796450f354b7c4aed4ffebd
mazhar-rony <mazhar.rony@gmail.com>
2021-02-02 09:04:38 +0600

    Author Dashboard Complete

--- resources/views/author/dashboard.blade.php
+++ resources/views/author/dashboard.blade.php
@@ -20,46 +20,94 @@
                     <i class="material-icons">playlist_add_check</i>
                 </div>
                 <div class="content">
-                    <div class="text">NEW TASKS</div>
-                    <div class="number count-to" data-from="0" data-to="125" data-speed="15" data-fresh-interval="20"></div>
+                    <div class="text">TOTAL POSTS</div>
+                    <div class="number count-to" data-from="0" data-to="{{ $posts->count() }}" data-speed="15" data-fresh-interval="20"></div>
                 </div>
             </div>
         </div>
         <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
             <div class="info-box bg-cyan hover-expand-effect">
                 <div class="icon">
-                    <i class="material-icons">help</i>
+                    <i class="material-icons">favorite</i>
                 </div>
                 <div class="content">
-                    <div class="text">NEW TICKETS</div>
-                    <div class="number count-to" data-from="0" data-to="257" data-speed="1000" data-fresh-interval="20"></div>
+                    <div class="text">TOTAL FAVORITE</div>
+                    <div class="number count-to" data-from="0" data-to="{{ Auth::user()->favorite_posts()->count() }}" data-speed="1000" data-fresh-interval="20"></div>
                 </div>
             </div>
         </div>
         <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
             <div class="info-box bg-light-green hover-expand-effect">
                 <div class="icon">
-                    <i class="material-icons">forum</i>
+                    <i class="material-icons">library_books</i>
                 </div>
                 <div class="content">
-                    <div class="text">NEW COMMENTS</div>
-                    <div class="number count-to" data-from="0" data-to="243" data-speed="1000" data-fresh-interval="20"></div>
+                    <div class="text">PENDING POSTS</div>
+                    <div class="number count-to" data-from="0" data-to="{{ $total_pending_posts }}" data-speed="1000" data-fresh-interval="20"></div>
                 </div>
             </div>
         </div>
         <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
             <div class="info-box bg-orange hover-expand-effect">
                 <div class="icon">
-                    <i class="material-icons">person_add</i>
+                    <i class="material-icons">visibility</i>
                 </div>
                 <div class="content">
-                    <div class="text">NEW VISITORS</div>
-                    <div class="number count-to" data-from="0" data-to="1225" data-speed="1000" data-fresh-interval="20"></div>
+                    <div class="text">TOTAL VIEWS</div>
+                    <div class="number count-to" data-from="0" data-to="{{ $all_views }}" data-speed="1000" data-fresh-interval="20"></div>
                 </div>
             </div>
         </div>
     </div>
     <!-- #END# Widgets -->
+    <div class="row clearfix">
+        <!-- Task Info -->
+        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+            <div class="card">
+                <div class="header">
+                    <h2>TOP 5 POPULAR POSTS</h2>
+                </div>
+                <div class="body">
+                    <div class="table-responsive">
+                        <table class="table table-hover dashboard-task-infos">
+                            <thead>
+                            <tr>
+                                <th>Rank List</th>
+                                <th>Title</th>
+                                <th>Views</th>
+                                <th>Favorite</th>
+                                <th>Comments</th>
+                                <th>Status</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                                @foreach($popular_posts as $key=>$post)
+                                    <tr>
+                                        <td>{{ $key + 1 }}</td>
+                                        <td>{{ str_limit($post->title,30) }}</td>
+                                        <td>{{ $post->view_count }}</td>
+                                        <td>{{ $post->favorite_to_users_count }}</td>
+                                        <td>{{ $post->comments_count }}</td>
+                                        <td>
+                                            @if($post->status == true)
+                                                <span class="label bg-green">Published</span>
+                                            @else
+                                                <span class="label bg-red">Pending</span>
+                                            @endif
+                                        </td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- #END# Task Info -->
+
+    </div>
+
+    
     <!-- CPU Usage -->
     <div class="row clearfix">
         <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
