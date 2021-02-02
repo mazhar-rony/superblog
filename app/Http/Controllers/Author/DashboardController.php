4cf8ffc45e5fe2b465384b28b61a8ac1f45a0308
mazhar-rony <mazhar.rony@gmail.com>
2021-02-02 11:58:56 +0600

    Admin Dashboard Complete

--- app/Http/Controllers/Author/DashboardController.php
+++ app/Http/Controllers/Author/DashboardController.php
@@ -17,8 +17,8 @@ class DashboardController extends Controller
                 ->withCount('comments')
                 ->withCount('favorite_to_users')
                 ->orderBy('view_count', 'desc')
-                ->orderBy('comments_count')
-                ->orderBy('favorite_to_users_count')
+                ->orderBy('comments_count', 'desc')
+                ->orderBy('favorite_to_users_count', 'desc')
                 ->take(5)->get();
 
         $total_pending_posts = $posts->where('is_approved', false)->count();
